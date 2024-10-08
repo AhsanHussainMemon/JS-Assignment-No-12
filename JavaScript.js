2e30f32cabe9836d2c7635eed051c2ef1f394086
AhsanHussainMemon <hussainahsan454@gmail.com>
2024-10-08 18:06:46 +0500

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,26 @@
+// Question No 1
+
+// var coinToss = Math.random(); 
+// var result; 
+
+// if (coinToss < 0.5) {
+//     result = "Heads"; 
+// } else {
+//     result = "Tails"; 
+// }
+
+// document.write("Random Coin Value : " + result);
+
+// Question No 2
+
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("Random Dice Value: " + diceValue);
+
+// Question No 3
+
+// var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+// var Password = '';
+// for (var i = 0; i < 10; i++) {
+//     Password += chars[Math.floor(Math.random() * chars.length)];
+// }
+// document.write("Password Is: " + Password);
